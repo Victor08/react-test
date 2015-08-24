82f2fdb44448cadeb05e4a10260e20ed898d2fc7
Victor Logunov <v.logunov@advmaker.net>
2015-08-24 10:27:48 +0300

    react touch event plugin added

--- resources/js/components/layout/Container.react.js
+++ resources/js/components/layout/Container.react.js
@@ -3,6 +3,8 @@ let FluxTweetApp = require('../FluxTweetApp.react');
 let NavigationBar = require('../layout/NavigationBar.react');
 let SlideMenuStore = require('../../stores/SlideMenuStore');
 let ReactCSSTransitionGroup = React.addons.CSSTransitionGroup;
+let SlideMenuActions = require('../../actions/FluxSlideMenuActions');
+(require("react-tap-event-plugin"))(); // injecting tap event plugin
 
 class Container extends BaseComponent {
     constructor(props){
@@ -20,16 +22,35 @@ class Container extends BaseComponent {
         })
     }
 
+    _closeMenu(){
+        console.log('close menu');
+        SlideMenuActions.toggleMenu(false);
+    }
+
+    // adjusts css of html and body if offcanvas menu is visible
+    _adjustCssOnWrapped(flag){
+        if (flag) {
+            $('html').css({'overflow': 'hidden'});
+            $('body').css({'overflow': 'hidden'});
+        } else {
+            $('html').css({'overflow': 'auto'});
+            $('body').css({'overflow': 'visible'});
+        }
+    }
+
     render(){
+        this._adjustCssOnWrapped(false);
         let container = (<div className="container">
             <NavigationBar/>
             <FluxTweetApp />
         </div>);
         if (this.state.wrapped) {
+            this._adjustCssOnWrapped(true);
             return (
                 <ReactCSSTransitionGroup transitionName="wrapper" transitionAppear="true">
                     <div id="wrapper">
-                    { container }
+                        <div id="overlay" onClick={this._closeMenu} onTouchTap={this._closeMenu}></div>
+                        { container }
                     </div>
                 </ReactCSSTransitionGroup>
             );
